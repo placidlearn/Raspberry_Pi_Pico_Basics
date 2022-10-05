f7a50968d3d9047454e994a0f8498462dadc2559
placidlearn <judenkereuwem@gmail.com>
2022-10-05 11:27:16 +0100

    code update

---
+++ Code/pico_OLED.py
@@ -0,0 +1,9 @@
+from machine import Pin, I2C
+from ssd1306 import SSD1306_I2C
+
+i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
+oled = SSD1306_I2C(128, 64, i2c)
+
+oled.text("  Welcome  to", 0, 16)
+oled.text("  Placidlearn", 0, 30)
+oled.show()
