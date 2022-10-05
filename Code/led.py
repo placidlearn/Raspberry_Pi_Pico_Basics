d82f559d908e86ca726bba14f5463ea9df618de6
placidlearn <judenkereuwem@gmail.com>
2022-10-05 11:32:02 +0100

    code update

--- Code/led.py
+++
@@ -1,8 +0,0 @@
-from machine import Pin, Timer
-led = Pin(15, Pin.OUT)
-timer = Timer()
-
-def blink(timer):
-    led.toggle()
-    
-timer.init(freq=2.5, mode=Timer.PERIODIC, callback=blink)
--- Code/onboardLED_blink.py
+++
@@ -1,6 +0,0 @@
-from machine import Pin
-led = Pin(25, Pin.OUT)
-
-#led.value(1) #led on
-#led.value(0) #led off
-led.toggle() #led on/off when Run is pressed
